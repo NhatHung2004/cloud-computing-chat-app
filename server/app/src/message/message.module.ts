d308863092d499127f47bcc39349aff97c8da42a
NhatHung <hung2004py@gmail.com>
2025-03-01 21:40:39 +0700

    setup realtime

---
+++ server/app/src/message/message.module.ts
@@ -0,0 +1,16 @@
+import { Module } from "@nestjs/common";
+import { MongooseModule } from "@nestjs/mongoose";
+import { MessageSchema, Message } from "./message.schema";
+import { MessageService } from "./message.service";
+import { MessageController } from "./message.controller";
+import { MessageGateway } from "./message.gateway";
+
+@Module({
+    imports: [
+        MongooseModule.forFeature([{ name: Message.name, schema: MessageSchema }]),
+    ],
+    providers: [MessageService, MessageGateway],
+    controllers: [MessageController],
+    exports: [MessageService],
+})
+export class MessageModule {}
