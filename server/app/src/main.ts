79653b032ac2a64d52053f930c13c51c03fb8801
NhatHung <hung2004py@gmail.com>
2025-03-05 21:10:29 +0700

    setup swagger

--- server/app/src/main.ts
+++ server/app/src/main.ts
@@ -1,8 +1,18 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import { SwaggerModule, DocumentBuilder } from '@nestjs/swagger';
 
 async function bootstrap() {
     const app = await NestFactory.create(AppModule);
+
+    const config = new DocumentBuilder()
+        .setTitle('NestJS API')
+        .setDescription('API description')
+        .setVersion('1.0')
+        .addTag('API')
+        .build();
+    const document = SwaggerModule.createDocument(app, config);
+    SwaggerModule.setup('swagger', app, document);
 
     // Kích hoạt CORS để client có thể kết nối từ domain khác
     app.enableCors();
