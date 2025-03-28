f23b6f4d25e86bc40734fdf2eb711f17c0627790
NhatHung <hung2004py@gmail.com>
2025-03-28 17:35:25 +0700

    Update client

---
+++ client/chat-app/src/app/Register/page.tsx
@@ -0,0 +1,38 @@
+import React from 'react'
+import '@/styles/register.css';
+import Link from 'next/link';
+
+const registerPage = () => {
+    return (
+        <div className='bg2'>
+            <div className='halfBg'>
+                <p className='textSignUp'>Create an account</p>
+                <p className='textLabel'>UserName</p>
+                <input id='userNameSignUp'
+                    type='text'
+                    className='inputUserNameSignUp'
+                />
+
+                <p className='textLabel'>Email</p>
+                <input id='emailSignUp'
+                    type='email'
+                    className='inputUserNameSignUp'
+                />
+
+                <p className='textLabel'>Password</p>
+                <input id='passwordSignUp'
+                    type='password'
+                    className='inputUserNameSignUp'
+                />
+                <button className='btnCreate'>Sign Up</button>
+                <div className='groupText'>
+                    <p className='textLabel text2'>Already have an account?</p>
+                    <Link href={"/login"} className='textLabel text2' >Sign In</Link>
+                </div>
+
+            </div>
+
+        </div>
+    )
+}
+export default registerPage
