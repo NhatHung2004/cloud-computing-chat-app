1e540ca4a5358ba7e9183ec90e02e0cc30b363a9
Nhật Hưng <hung2004py@gmail.com>
2025-04-20 21:17:36 +0700

    Update s3.config.ts

--- server/app/src/config/s3.config.ts
+++ server/app/src/config/s3.config.ts
@@ -3,8 +3,8 @@ import { S3Client } from "@aws-sdk/client-s3";
 export const s3Client = new S3Client({
     region: "us-east-1",
     credentials: {
-        accessKeyId: "ASIAUUCWGLE3B5GIK6GY",
-        secretAccessKey: "SPDreN2inUxqPWGP/89SqjGy9+s+pST+3f7j9K4q",
-        sessionToken: "IQoJb3JpZ2luX2VjEBgaCXVzLXdlc3QtMiJGMEQCICbZzfWi3ISBtoP7HSYG/mly2lTMzjtPK1YyTC202GN8AiA+HV0UMzeMcbNjV1Ha84GtNBokJqXdxZhaGaQncSDa3yq4Agih//////////8BEAEaDDMxODAwODM1MTAzMCIMxrINOMNtaE+G4FsQKowCXQ0gregLNWI878gXQSPjQAKF0z5O4umQ3XNsvAPFQowj/MlLipvxh6z34vUjUn9bHPJiPe73gkurvumfaqUASsgc3lHqzDYoPes00FxnJcBlpSMwMLrnwnivDsWynxNDYNGbKOmUcNzKlpHCS7ilVjcVEjkCUJQvKOaAYTGlCDl7NyyU625U72WgwNwWMjFNeVAv0inyiFrsyKrHdZoAosZBXIWOlhOoN2CtBy1NURc7ATvhDFV9q/rZ0385jWrmY/w36rt5QicXexizBnejXr04AnVsUZXNsWAt5Ngae46iru3BclaAQ9qEUeTmX7tLYEgZNQykvm4UVFfZvRgE6cPtWgFLkEuc9zTFHzDlzpLABjqeAdJ+H0Km+g7LnsSPmf8z7qctZ2DgdEzINTqtiL5TSJ+cQrbMFdK/mr7GL7YSXldFjsv4x7IZPGCytbsNOktWGB55jsn/HpES9K+Ry1/FxHHpTKVjrpzOmrs0LzFeaSrj8uOGK6zV3TYu1IuJVFDM9YXIXjhDu244i4cBbUbDuVYshrMYWzFS19sCpwfXwd5jjnLLtGbnG/cR6aTfUJAP",
+        accessKeyId: "ASIAUUCWGLE3AFTPI5LL",
+        secretAccessKey: "GirXaqzv6hnV7zoW8h672SaloMn8RL5AXc4uLy7Q",
+        sessionToken: "IQoJb3JpZ2luX2VjEB4aCXVzLXdlc3QtMiJGMEQCIDCuNGqdet77Boi/NL/7UK3U1OLKkyN3G4se7VW7RFImAiBavYwJbBgSHmnMnnaD20hYjDCEjU8g6kbtbDrGC/fwOiq4Agin//////////8BEAEaDDMxODAwODM1MTAzMCIM2InyTYZ5Gfrx6jNxKowC81KOWMoRj0MJK3hy4Im7aa7YOZekeEOpotltB6x0aNoGMGjDi5gZC2jERtPoyJZlh1joyeoRablTk2u6CHPGZHud6A+NKz3x3RVLh70uV2hHnzQ9S2a67+wkv9+PYN1SLQUAsBUvu47mWlAuau9MtFImw21LgK2bIc4kl2HklXLsrDnVWf7p0+Ju/Z1PuLnZ8oMvIbJ4YSYruhQt+kE+rzGhx297MzkpzrimDS4BfVwnlUZScLX5LiyKa4rYWl6rUXFrHs1NSLDZmU9aq9ABuzqzAod4tOqCgjdRAukazncd8FBb6K2r+Ej+oxE3tQQjlcgvbTtRZlx5EepheOmra3BMxz0cfw/GerFZ6zDh/5PABjqeAZJRQA9YVHKfMA/6GNlQMW9bhGIZb+9o9fbwkgBEbZ9jd0GImL6wp581OPoi/y0lBnSM0bjwUudz2mIK8syoP4M/l2vHufXvOxeSrsYhNaNH9j+21dWfN2aHMPLddi7s4co6rt24E77NXxxpoiwojl5VVEEXVCF7KNEqqKCde60Ywvx8QCZnWKEUvkqsMr37fFWN3sYhNazIV2uHKXhw",
     },
 });
